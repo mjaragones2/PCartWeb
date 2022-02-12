b23e376ef7aeefd1ddd362d6b0d0a49ef0cdca33
Mark Joshua Aragones <aragones.markjoshua25425@gmail.com>
2022-02-12 21:26:16 +0800

    New update

---
+++ PCartWeb/Migrations/Configuration.cs
@@ -0,0 +1,56 @@
+﻿namespace PCartWeb.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using PCartWeb.Models;
+    using System;
+    using System.Configuration;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    internal sealed class Configuration : DbMigrationsConfiguration<PCartWeb.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(PCartWeb.Models.ApplicationDbContext context)
+        {
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            string name = "PCartTeam@gmail.com";
+            string password = "PcartTeam@2021";
+            const string roleName = "Admin";
+            var role = roleManager.FindByName(roleName);
+            if (role == null)
+            {
+                role = new IdentityRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = name,
+                    Email = name,
+                    EmailConfirmed = true
+                };
+                var result = userManager.Create(user, password);
+                if (result.Succeeded)
+                {
+                    result = userManager.SetLockoutEnabled(user.Id, false);
+                }
+            }
+
+            var rolesForuser = userManager.GetRoles(user.Id);
+            if (!rolesForuser.Contains(role.Name))
+            {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+}
